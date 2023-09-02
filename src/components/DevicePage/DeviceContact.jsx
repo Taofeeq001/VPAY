9022570a04427cf759c59fd8e0a189ae04626770
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-02 19:14:13 -0400

    updated device page

--- src/components/DevicePage/DeviceContact.jsx
+++ src/components/DevicePage/DeviceContact.jsx
@@ -6,8 +6,8 @@ const DeviceContact = () => {
     <div className='px-[10px] my-[25px] lg:px-[30px]'>
         <div className='rounded-lg bg-white relative py-[10%]'>
             <Image className='absolute left-0 top-0' width={400} height={400} src="/Images/left.svg" />
-            <div className='flex flex-col items-center'>
-                <h1>Want to Order for Your Device?</h1>
+            <div className='flex flex-col items-center text gap-4 w-full'>
+                <h1 className='w-[20%] text-center text-[#00CCFF]'>Want to Order for Your Device?</h1>
                 <button className='btn'>Contact Us</button>
             </div>
             <Image  className='absolute right-0 top-0' width={400} height={400} src="/Images/right.svg"/>
