89d3080bdabc39cb9a2413b2938841f7db338236
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-02 18:23:54 -0400

    updating device page

--- src/components/DevicePage/DeviceContact.jsx
+++ src/components/DevicePage/DeviceContact.jsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 
 const DeviceContact = () => {
   return (
-    <div className='px-[10px] mt-[25px] lg:px-[30px]'>
+    <div className='px-[10px] my-[25px] lg:px-[30px]'>
         <div className='rounded-lg bg-white relative py-[10%]'>
             <Image className='absolute left-0 top-0' width={400} height={400} src="/Images/left.svg" />
             <div className='flex flex-col items-center'>
                 <h1>Want to Order for Your Device?</h1>
                 <button className='btn'>Contact Us</button>
             </div>
-            <Image  className='absolute right-0' width={100} height={100} src="/Images/right.svg"/>
+            <Image  className='absolute right-0 top-0' width={400} height={400} src="/Images/right.svg"/>
         </div>
     </div>
   )
